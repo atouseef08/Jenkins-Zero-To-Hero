d05f6ccb4bab4964231a860814d225d421509ebf
Ubuntu <ubuntu@ip-172-31-39-238.ec2.internal>
2024-01-30 14:22:41 +0000

    test

--- multi-stage-multi-agent-assignment/src/hello-world.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        String name = "Ali Touseef";
-        System.out.println("Hello This is, " + name + "!");
-    }
-}
-
